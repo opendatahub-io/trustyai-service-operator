eab6554ef61b4d1d97a2706ae3cb310e662618c5
Rob Geada <rob@geada.net>
2025-11-18 12:37:04 +0000

    Add env variable specification to GuardrailsOrch API (#602)
    (cherry picked from commit 6c52ee8de962f7344d3c0404ecdb6b6d259f2141)
    
    # Conflicts:
    #	api/gorch/v1alpha1/guardrailsorchestrator_types.go
    #	api/gorch/v1alpha1/zz_generated.deepcopy.go

--- api/gorch/v1alpha1/guardrailsorchestrator_types.go
+++ api/gorch/v1alpha1/guardrailsorchestrator_types.go
@@ -64,6 +64,14 @@ type GuardrailsOrchestratorSpec struct {
 	// Set log level in the orchestrator deployment
 	// +optional
 	LogLevel *string `json:"logLevel,omitempty"`
+<<<<<<< HEAD
+=======
+	// Define TLS secrets to be mounted to the orchestrator. Secrets will be mounted at /etc/tls/$SECRET_NAME
+	// +optional
+	TLSSecrets *[]string `json:"tlsSecrets,omitempty"`
+	// Define environment variables. These will be added to the orchestrator, gateway, and built-in detector pods.
+	EnvVars *[]corev1.EnvVar `json:"env,omitempty"`
+>>>>>>> 6c52ee8 (Add env variable specification to GuardrailsOrch API (#602))
 }
 
 // OtelExporter defines the environment variables for configuring the OTLP exporter.
